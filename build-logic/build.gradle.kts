3d9cea62437cff2b131255ce5357fcf8986151bc
taetae98coding <taetae98coding@gmail.com>
2023-12-07 22:33:21 +0900

    JVM build-logic

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -23,6 +23,11 @@ gradlePlugin {
             implementationClass = "plugin.ios.IosPlugin"
         }
 
+        register("diary.jvm") {
+            id = "diary.jvm"
+            implementationClass = "plugin.jvm.JvmPlugin"
+        }
+
         register("diary.js") {
             id = "diary.js"
             implementationClass = "plugin.js.JsPlugin"
--- target/jvmApp/build.gradle.kts
+++ target/jvmApp/build.gradle.kts
@@ -1,11 +1,6 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    id("diary.jvm")
     alias(libs.plugins.compose.multiplatform)
-}
-
-kotlin {
-    explicitApi()
-    jvmToolchain(17)
 }
 
 dependencies {
