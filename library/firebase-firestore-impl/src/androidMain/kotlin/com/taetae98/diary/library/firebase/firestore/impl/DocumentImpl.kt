7988a43469aa91d954d4af4f3587daef0e8c8f61
taetae98coding <taetae98coding@gmail.com>
2023-12-24 01:32:24 +0900

    android remove await

--- library/firebase-firestore-impl/src/androidMain/kotlin/com/taetae98/diary/library/firebase/firestore/impl/DocumentImpl.kt
+++ library/firebase-firestore-impl/src/androidMain/kotlin/com/taetae98/diary/library/firebase/firestore/impl/DocumentImpl.kt
@@ -2,12 +2,11 @@ package com.taetae98.diary.library.firebase.firestore.impl
 
 import com.google.firebase.firestore.DocumentReference
 import com.taetae98.diary.library.firestore.api.Document
-import kotlinx.coroutines.tasks.await
 
 internal class DocumentImpl(
     private val document: DocumentReference
 ) : Document {
     override suspend fun upsert(entity: Any) {
-        document.set(entity).await()
+        document.set(entity)
     }
 }
