49e56808739e3c5cd124fb91b3212d69d51947be
taetae98coding <taetae98coding@gmail.com>
2024-01-02 20:17:04 +0900

    요일 적용

--- library/calendar-compose/src/commonMain/kotlin/com/taetae98/diary/library/calendar/compose/Calendar.kt
+++ library/calendar-compose/src/commonMain/kotlin/com/taetae98/diary/library/calendar/compose/Calendar.kt
@@ -1,15 +1,82 @@
 package com.taetae98.diary.library.calendar.compose
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import com.taetae98.diary.library.calendar.compose.month.Month
+import com.taetae98.diary.library.calendar.compose.provider.LocalWeekSaturdayColor
+import com.taetae98.diary.library.calendar.compose.provider.LocalWeekSundayColor
+import kotlinx.datetime.DayOfWeek
+
+@Composable
+public fun Calendar(
+    modifier: Modifier = Modifier,
+    state: CalendarState,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        DayOfWeek()
+        Content(
+            modifier = Modifier.weight(1F),
+            state = state
+        )
+    }
+
+}
+
+@Composable
+private fun DayOfWeek(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+    ) {
+        repeat(7) {
+            val dayOfWeek = when (it) {
+                0 -> DayOfWeek.SUNDAY
+                else -> DayOfWeek(it)
+            }
+
+            val text = when (dayOfWeek) {
+                DayOfWeek.SUNDAY -> "일"
+                DayOfWeek.MONDAY -> "월"
+                DayOfWeek.TUESDAY -> "화"
+                DayOfWeek.WEDNESDAY -> "수"
+                DayOfWeek.THURSDAY -> "목"
+                DayOfWeek.FRIDAY -> "금"
+                DayOfWeek.SATURDAY -> "토"
+                else -> ""
+            }
+
+            val color = when (dayOfWeek) {
+                DayOfWeek.SUNDAY -> LocalWeekSundayColor.current
+                DayOfWeek.SATURDAY -> LocalWeekSaturdayColor.current
+                else -> LocalContentColor.current
+            }
+
+            Text(
+                modifier = Modifier.weight(1F),
+                text = text,
+                color = color,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleSmall,
+            )
+        }
+    }
+}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-public fun Calendar(
+private fun Content(
     modifier: Modifier = Modifier,
     state: CalendarState,
 ) {
--- library/calendar-compose/src/commonMain/kotlin/com/taetae98/diary/library/calendar/compose/week/WeekDay.kt
+++ library/calendar-compose/src/commonMain/kotlin/com/taetae98/diary/library/calendar/compose/week/WeekDay.kt
@@ -1,12 +1,11 @@
 package com.taetae98.diary.library.calendar.compose.week
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import com.taetae98.diary.library.calendar.compose.provider.LocalWeekSaturdayColor
 import com.taetae98.diary.library.calendar.compose.provider.LocalWeekSundayColor
 import kotlinx.datetime.DayOfWeek
@@ -16,26 +15,23 @@ public fun WeekDay(
     modifier: Modifier = Modifier,
     state: WeekDayState,
 ) {
-    Box(
-        modifier = modifier,
-        contentAlignment = Alignment.Center
-    ) {
-        val color = when (state.dayOfWeek) {
-            DayOfWeek.SUNDAY -> LocalWeekSundayColor.current
-            DayOfWeek.SATURDAY -> LocalWeekSaturdayColor.current
-            else -> LocalContentColor.current
-        }.copy(
-            alpha = if (state.isSameMonth()) {
-                1F
-            } else {
-                0.38F
-            }
-        )
+    val color = when (state.dayOfWeek) {
+        DayOfWeek.SUNDAY -> LocalWeekSundayColor.current
+        DayOfWeek.SATURDAY -> LocalWeekSaturdayColor.current
+        else -> LocalContentColor.current
+    }.copy(
+        alpha = if (state.isSameMonth()) {
+            1F
+        } else {
+            0.38F
+        }
+    )
 
-        Text(
-            text = state.localDate.dayOfMonth.toString(),
-            color = color,
-            style = MaterialTheme.typography.titleSmall
-        )
-    }
+    Text(
+        modifier = modifier,
+        text = state.localDate.dayOfMonth.toString(),
+        color = color,
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.titleSmall
+    )
 }
