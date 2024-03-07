d29056ee9294d22eb97136385ee75ef74f6f5ac2
taetae98coding <taetae98coding@gmail.com>
2024-03-07 18:56:17 +0900

    PageTagUseCase

--- domain/usecase/src/commonMain/kotlin/com/taetae98/diary/domain/usecase/tag/PageTagUseCase.kt
+++ domain/usecase/src/commonMain/kotlin/com/taetae98/diary/domain/usecase/tag/PageTagUseCase.kt
@@ -3,26 +3,32 @@ package com.taetae98.diary.domain.usecase.tag
 import app.cash.paging.PagingData
 import com.taetae98.diary.domain.entity.account.Account
 import com.taetae98.diary.domain.entity.tag.Tag
+import com.taetae98.diary.domain.exception.NoAccountException
 import com.taetae98.diary.domain.repository.TagRepository
 import com.taetae98.diary.domain.usecase.account.GetAccountUseCase
 import com.taetae98.diary.domain.usecase.core.FlowUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapLatest
 import org.koin.core.annotation.Factory
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @Factory
 public class PageTagUseCase internal constructor(
     private val getAccountUseCase: GetAccountUseCase,
     private val tagRepository: TagRepository,
 ) : FlowUseCase<Unit, PagingData<Tag>>() {
-    @OptIn(ExperimentalCoroutinesApi::class)
     override fun execute(params: Unit): Flow<Result<PagingData<Tag>>> {
-        return getAccountUseCase(Unit).mapLatest(Result<Account>::getOrThrow)
-            .mapLatest { it.uid }
-            .flatMapLatest(tagRepository::page)
+        return getAccountUseCase(Unit).map(Result<Account>::getOrNull)
+            .flatMapLatest(::flatMapAccount)
+    }
+
+    private fun flatMapAccount(account: Account?): Flow<Result<PagingData<Tag>>> {
+        if (account == null) return flowOf(Result.failure(NoAccountException()))
+
+        return tagRepository.page(account.uid)
             .map { Result.success(it) }
     }
 }
