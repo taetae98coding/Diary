e7f6bff1026fe966dd8258d94a3ea3101bc05150
taetae98coding <taetae98coding@gmail.com>
2024-03-07 18:20:16 +0900

    FindTagInMemoUseCase

--- domain/usecase/src/commonMain/kotlin/com/taetae98/diary/domain/usecase/tag/select/FindTagInMemoUseCase.kt
+++ domain/usecase/src/commonMain/kotlin/com/taetae98/diary/domain/usecase/tag/select/FindTagInMemoUseCase.kt
@@ -1,24 +1,33 @@
 package com.taetae98.diary.domain.usecase.tag.select
 
+import com.taetae98.diary.domain.entity.account.Account
 import com.taetae98.diary.domain.entity.tag.Tag
+import com.taetae98.diary.domain.exception.NoAccountException
 import com.taetae98.diary.domain.repository.SelectTagByMemoRepository
 import com.taetae98.diary.domain.usecase.account.GetAccountUseCase
 import com.taetae98.diary.domain.usecase.core.FlowUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import org.koin.core.annotation.Factory
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @Factory
 public class FindTagInMemoUseCase internal constructor(
-    private val selectTagByMemoRepository: SelectTagByMemoRepository,
     private val getAccountUseCase: GetAccountUseCase,
+    private val selectTagByMemoRepository: SelectTagByMemoRepository,
 ) : FlowUseCase<Unit, List<Tag>>() {
     override fun execute(params: Unit): Flow<Result<List<Tag>>> {
-        return getAccountUseCase(Unit).map { it.getOrThrow() }
-            .flatMapLatest { selectTagByMemoRepository.find(it.uid) }
+        return getAccountUseCase(Unit).map { it.getOrNull() }
+            .flatMapLatest(::flatMapAccount)
+    }
+
+    private fun flatMapAccount(account: Account?): Flow<Result<List<Tag>>> {
+        if (account == null) return flowOf(Result.failure(NoAccountException()))
+
+        return selectTagByMemoRepository.find(account.uid)
             .map { Result.success(it) }
     }
 }
